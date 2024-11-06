c2131e0390f3aff1115faf2bc50c3b8379ff0399
Barış Yıldırım <98666277+BarisYildirim6@users.noreply.github.com>
2024-11-06 21:05:43 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "Hi, 22003175!"
+    return "Hi, 22003175! This is branch_v2"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
